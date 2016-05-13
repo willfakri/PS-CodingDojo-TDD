32a38c07a4ed80b9e5c00172c133f76944ed2b79
Willian Fakri <wfakri@ciandt.com>
2016-05-13 12:12:44 -0300

    Finalizado. Passou os 4 testes, refatorado.

--- 2-KaraLeapYear/test_leapYear.py
+++ 2-KaraLeapYear/test_leapYear.py
@@ -1,9 +1,25 @@
-
 def isLeapYear(year):
-	return year % 4 == 0;
+	if(isDivisibleBy4(year)):		
+		return True
+	return isDivisibleBy400(year)
+	
+def isDivisibleBy4(year):
+	return year % 4 == 0 and year % 100 != 0
+	
+def isDivisibleBy400(year):
+	return year % 400 == 0
 
+
+	
+# # # TESTS # # #	
 def test_simple_leapYear():
 	assert isLeapYear(1996)
 	
 def test_simple_not_leapYear():
 	assert not isLeapYear(2001)
+	
+def test_divisible_by_100():
+	assert isLeapYear(2000)
+	
+def test_divisible_by_400():
+	assert not isLeapYear(1900)
